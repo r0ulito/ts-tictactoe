dcdf73c6e57487745358da20c1cdaf6df221709c
= <julien.jovy@gmail.com>
2020-07-06 12:57:13 +0200

    third-step | Check if winner && gameEnd functions implementation

--- 3/main.ts
+++ 3/main.ts
@@ -1,3 +1,7 @@
+interface GameState {
+    index :number | string,
+    player :string
+}
 let startBoard :Array<string | number> = [];
 const humanPlayer :string = "O";
 const botPlayer :string = "X";
@@ -38,4 +42,38 @@ function playerAction(id, player :string) :void {
     cell.removeEventListener('click', playerClickAction);
     cell.innerText = player;
     startBoard[id] = player;
+    const gameState :GameState | null = isPlayerWinner(startBoard, player);
+    if(gameState) {
+        gameEnd(gameState);
+    }
+}
+
+function isPlayerWinner(board :(string | number)[], player : string) :GameState {
+    const plays :Array<number> = board.reduce((a :Array<number>, e, i) => (e === player) ? a.concat(i): a, []);
+    console.log(plays);
+    let gameState :GameState = null;
+    for(const [index, winning] of winningCombinations.entries()) {
+        if(winning.every(elem => plays.indexOf(elem) > -1)) {
+            gameState = {
+                index: index,
+                player: player
+            };
+            break;
+        }
+    }
+    return gameState;
+}
+
+
+function gameEnd(gameState :GameState) :void {
+    const color :string = (gameState.player === humanPlayer ? "lightblue": "salmon");
+    cells.forEach((el) => {
+        el.removeEventListener('click', playerClickAction);
+    });
+    for(const index of winningCombinations[gameState.index]) {
+        const cell = document.getElementById(index);
+        cell.style.backgroundColor = color;
+    }
+    winnerChildEl.innerText = "You win";
+    winnerParentEl.style.display = "block";
 }
